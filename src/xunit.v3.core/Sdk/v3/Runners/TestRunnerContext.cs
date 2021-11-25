e0a5179c25dd6aa4b6ccaf700c8869a923aa7a23
Brad Wilson <dotnetguy@gmail.com>
2021-11-25 14:55:11 -0800

    Make TestRunner and XunitTestRunner singletons for performance

---
+++ src/xunit.v3.core/Sdk/v3/Runners/TestRunnerContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using Xunit.Sdk;
+
+namespace Xunit.v3;
+
+/// <summary>
+/// Base context record for <see cref="TestRunner{TContext}"/>
+/// </summary>
+public record TestRunnerContext(
+	_ITest Test,
+	IMessageBus MessageBus,
+	Type TestClass,
+	object?[] ConstructorArguments,
+	MethodInfo TestMethod,
+	object?[]? TestMethodArguments,
+	string? SkipReason,
+	ExceptionAggregator Aggregator,
+	CancellationTokenSource CancellationTokenSource
+);
---
+++ src/xunit.v3.core/Sdk/v3/Runners/XunitTestRunnerContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using Xunit.Sdk;
+
+namespace Xunit.v3;
+
+/// <summary>
+/// Context record for <see cref="XunitTestRunner"/>.
+/// </summary>
+public record XunitTestRunnerContext : TestRunnerContext
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="XunitTestRunnerContext"/> record.
+	/// </summary>
+	public XunitTestRunnerContext(
+		_ITest test,
+		IMessageBus messageBus,
+		Type testClass,
+		object?[] constructorArguments,
+		MethodInfo testMethod,
+		object?[]? testMethodArguments,
+		string? skipReason,
+		ExceptionAggregator aggregator,
+		CancellationTokenSource cancellationTokenSource,
+		IReadOnlyCollection<BeforeAfterTestAttribute> beforeAfterTestAttributes) :
+			base(test, messageBus, testClass, constructorArguments, testMethod, testMethodArguments, skipReason, aggregator, cancellationTokenSource)
+	{
+		BeforeAfterTestAttributes = beforeAfterTestAttributes;
+	}
+
+	/// <summary>
+	/// Gets or sets the collection of <see cref="BeforeAfterTestAttribute"/> used for this test.
+	/// </summary>
+	public IReadOnlyCollection<BeforeAfterTestAttribute> BeforeAfterTestAttributes { get; init; }
+}
